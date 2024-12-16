c6045e510959d1532d19f450be048ff6314b8cfd
aliakseidrob <aliakseidrob@gmail.com>
2024-12-16 15:15:14 +0300

    refactor(advanced-2.1): add BEM notations to form class names.

--- app.js
+++ app.js
@@ -35,19 +35,19 @@ var createNewTaskElement=function(taskString){
     listItem.className = "task-item";
 
     label.innerText=taskString;
-    label.className='task-label';
+    label.className='task-item__label';
 
     //Each elements, needs appending
     checkBox.type="checkbox";
-    checkBox.className="task-checkbox";
+    checkBox.className="task-item__checkbox";
     editInput.type="text";
-    editInput.className="task-input";
+    editInput.className="task-item__input";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
     editButton.className="button edit-btn";
 
     deleteButton.className="button delete-btn";
-    deleteButtonImg.className='img';
+    deleteButtonImg.className='delete-btn__img';
     deleteButtonImg.src='./remove.svg';
     deleteButtonImg.alt='remove';
     deleteButton.appendChild(deleteButtonImg);
@@ -87,8 +87,8 @@ var editTask=function(){
 
     var listItem=this.parentNode;
 
-    var editInput=listItem.querySelector('.task-input');
-    var label=listItem.querySelector(".task-label");
+    var editInput=listItem.querySelector('.task-item__input');
+    var label=listItem.querySelector(".task-item__label");
     var editBtn=listItem.querySelector(".edit-btn");
     var containsClass=listItem.classList.contains("edit-mode");
     //If class of the parent is .edit-mode
@@ -160,7 +160,7 @@ addButton.addEventListener("click",ajaxRequest);
 var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
     console.log("bind list item events");
 //select ListItems children
-    var checkBox=taskListItem.querySelector(".task-checkbox");
+    var checkBox=taskListItem.querySelector(".task-item__checkbox");
     var editButton=taskListItem.querySelector(".edit-btn");
     var deleteButton=taskListItem.querySelector(".delete-btn");
 
